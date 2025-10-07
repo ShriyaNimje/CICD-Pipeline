29d361af8f86a4ad94b5f1a5bcb61dc81e74d0ba
shriyanimje <shriyanimje2002@gamil.com>
2025-10-07 20:03:48 +0530

    Python Flask app ready with Docker init

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return '''
+    <html>
+      <head>
+        <title>CI/CD Pipeline</title>
+        <style>
+          body {
+            background-color: #72bf6a; /* Light blue background */
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: Arial, sans-serif;
+          }
+          h1 {
+            color: #333;
+          }
+        </style>
+      </head>
+      <body>
+        <h1>CI/CD Pipeline Created Successfully</h1>
+      </body>
+    </html>
+    '''
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
